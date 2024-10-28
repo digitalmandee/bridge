8b970911a8d05ee668a9700dda900d14c79f5556
yasirCodes12 <122172335+yasirCodes12@users.noreply.github.com>
2024-10-28 19:48:26 +0500

    edit member layout .

--- resources/views/admin/member/edit.blade.php
+++ resources/views/admin/member/edit.blade.php
@@ -3,45 +3,106 @@
 @section('content')
 <div class="page-content">
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
-        <div>
+        <div class="d-flex align-items-baseline gap-2">
+            <a href="#"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
             <h3 class="mb-3 mb-md-0">Edit Member</h3>
         </div>
     </div>
-    <div class="card shadow-sm mx-auto">
-        <form action="{{ route('admin.branch.update', $branch->id) }}" method="POST" class="card-body row">
-            @csrf
-            <div class="col-6 col-md-12 col-xl-6 mb-2">
-                <label class="mb-1" for="name">Branch Name</label>
-                <div class="form-group">
-                    <input type="text" name="name" value="{{ $branch->name }}" class="form-control">
+    <div class="row justify-content-center">
+        <div class="card col-6 col-md-12 col-xl-6">
+            <form action="#" class="card-body">
+                <div class="grid-margin">
+                    <h3 class="mb-3 mb-md-0">Personal Details</h3>
                 </div>
-            </div>
-
-            <div class="col-6 col-md-12 col-xl-6 mb-2">
-                <label class="mb-1" for="name">Branch Location</label>
-                <div class="form-group">
-                    <input type="text" name="location" value="{{ $branch->location }}" class="form-control">
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Name</label>
+                    <div class="form-group">
+                        <input type="text" name="name" class="form-control" placeholder="Name">
+                        <small class="text-secondary">Name or company are required</small>
+                    </div>
                 </div>
-            </div>
-            <div class="col-6 col-md-12 col-xl-6 mb-2 d-flex align-items-end gap-5">
-                <label for="name">Status</label>
-                <div class="checkbox-wrapper-3 form-group">
-                    <input id="cbx-3" type="checkbox" name="status" value="1" {{ $branch->status == 1 ? 'checked' : '' }}>
-                    <label for="cbx-3" class="toggle"><span></span></label>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Company</label>
+                    <div class="form-group">
+                        <input type="text" name="name" class="form-control" placeholder="Company">
+                    </div>
                 </div>
-            </div>
-            <div class="d-flex justify-content-center mt-4">
-                <a href="{{ route('dashboard') }}" class="btn-secondary me-2">Cancel</a>
-                <button type="submit" class="btn-success">Update</button>
-            </div>
-        </form>
-        <!-- SweetAlert trigger -->
-        @if (session('error'))
-        <script>
-            showToast('error', '{{ session('
-                error ') }}', '#f8d7da');
-        </script>
-        @endif
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Address*</label>
+                    <div class="form-group">
+                        <textarea name="name" class="form-control" id="" cols="30" rows=""></textarea>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Phone</label>
+                    <div class="form-group">
+                        <input type="tel" name="name" class="form-control" placeholder="+92">
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Email</label>
+                    <div class="form-group">
+                        <input type="tel" name="name" class="form-control" placeholder="Eg @gamil.com">
+                    </div>
+                </div>
+                <div class="grid-margin">
+                    <h3 class="mb-3 mb-md-0">Membership Details</h3>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Total Menbers</label>
+                    <div class="form-group">
+                        <input type="number" name="name" class="form-control" placeholder="00">
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Start Date</label>
+                    <div class="form-group">
+                        <input type="date" name="name" class="form-control" value="2024-10-28">
+                        <small class="text-secondary">When Should be membership start</small>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">First Recurring Invoice Date</label>
+                    <div class="form-group">
+                        <input type="date" name="name" class="form-control" value="2024-10-28">
+                        <small class="text-secondary">When should the first invoice be sent?</small>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Send prorated invoice on start date</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Selecting this will allow you to customize the member's plan or remove a sign-up charge before confirming them and generating their first invoice.</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Send confirmation email to member</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Add Note</label>
+                    <div class="form-group">
+                        <textarea name="name" class="form-control" id="" cols="30" rows=""></textarea>
+                    </div>
+                </div>
+                <div class="box-footer text-center">
+                    <button class="layout-btn active">Update Member</button>
+                </div>
+            </form>
+        </div>
     </div>
+    <!-- SweetAlert trigger -->
+    @if (session('error'))
+    <script>
+        showToast('error', '{{ session('error') }}', '#f8d7da');
+    </script>
+    @endif
 </div>
 @endsection
