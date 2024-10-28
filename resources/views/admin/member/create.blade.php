4429e29758b820ecc1bc6142d111868838f5bdf3
yasirCodes12 <122172335+yasirCodes12@users.noreply.github.com>
2024-10-28 19:35:29 +0500

    members layout

--- resources/views/admin/member/create.blade.php
+++ resources/views/admin/member/create.blade.php
@@ -3,18 +3,107 @@
 @section('content')
 <div class="page-content">
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
-        <div>
-            <h3 class="mb-3 mb-md-0">Create Member</h3>
+        <div class="d-flex align-items-baseline gap-2">
+            <a href="#"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+            <h3 class="mb-3 mb-md-0">Add Member</h3>
         </div>
     </div>
-    <div class="card shadow-sm mx-auto">
-        <!-- SweetAlert trigger -->
-        @if (session('error'))
-        <script>
-            showToast('error', '{{ session('
-                error ') }}', '#f8d7da');
-        </script>
-        @endif
+    <div class="row justify-content-center">
+        <div class="card col-6 col-md-12 col-xl-6">
+            <form action="#" class="card-body">
+                <div class="grid-margin">
+                    <h3 class="mb-3 mb-md-0">Personal Details</h3>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Name</label>
+                    <div class="form-group">
+                        <input type="text" name="name" class="form-control" placeholder="Name">
+                        <small class="text-secondary">Name or company are required</small>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Company</label>
+                    <div class="form-group">
+                        <input type="text" name="name" class="form-control" placeholder="Company">
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Address*</label>
+                    <div class="form-group">
+                        <textarea name="name" class="form-control" id="" cols="30" rows=""></textarea>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Phone</label>
+                    <div class="form-group">
+                        <input type="tel" name="name" class="form-control" placeholder="+92">
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Email</label>
+                    <div class="form-group">
+                        <input type="tel" name="name" class="form-control" placeholder="Eg @gamil.com">
+                    </div>
+                </div>
+                <div class="grid-margin">
+                    <h3 class="mb-3 mb-md-0">Membership Details</h3>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Total Menbers</label>
+                    <div class="form-group">
+                        <input type="number" name="name" class="form-control" placeholder="00">
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Start Date</label>
+                    <div class="form-group">
+                        <input type="date" name="name" class="form-control" value="2024-10-28">
+                        <small class="text-secondary">When Should be membership start</small>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">First Recurring Invoice Date</label>
+                    <div class="form-group">
+                        <input type="date" name="name" class="form-control" value="2024-10-28">
+                        <small class="text-secondary">When should the first invoice be sent?</small>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Send prorated invoice on start date</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Selecting this will allow you to customize the member's plan or remove a sign-up charge before confirming them and generating their first invoice.</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="" value="1" class="form-check-input">
+                        <label class="form-check-label">Send confirmation email to member</label>
+                    </div>
+                </div>
+                <div class="col-6 col-md-12 col-xl-12 mb-2">
+                    <label class="mb-1" for="name">Add Note</label>
+                    <div class="form-group">
+                        <textarea name="name" class="form-control" id="" cols="30" rows=""></textarea>
+                    </div>
+                </div>
+                <div class="box-footer text-center">
+                    <button class="layout-btn active">Add Members</button>
+                </div>
+            </form>
+        </div>
     </div>
+    <!-- SweetAlert trigger -->
+    @if (session('error'))
+    <script>
+        showToast('error', '{{ session('
+            error ') }}', '#f8d7da');
+    </script>
+    @endif
 </div>
 @endsection
--- resources/views/admin/member/index.blade.php
+++ resources/views/admin/member/index.blade.php
@@ -1,65 +1,99 @@
 @extends('admin.master')
 @section('title', __('Members'))
 @section('content')
-    <div class="page-content">
+<div class="page-content">
+    <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
         <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
-            <div>
+            <div class="d-flex align-items-baseline gap-2">
+                <a href="#"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
                 <h3>Members</h3>
             </div>
+        </div>
+        <div class="d-flex align-items-center gap-2">
+            <a href="#" class="text-dark"><i class="fa fa-download text-warning"></i> CSV</a>
             <a href="{{ route('admin.members.create') }}" class="layout-btn active">Add Members</a>
         </div>
-        <div class="row card col-md-12">
-            <table class="table table-responsive">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Location</th>
-                        <th>Status</th>
-                        <th class="text-center">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {{-- @php($sl = 1)
-                    @forelse ($branches as $branch)
-                        <tr>
-                            <td>{{ $sl++ }}</td>
-                            <td>{{ $branch->name }}</td>
-                            <td>{{ $branch->location }}</td>
-                            <td>{{ $branch->status == 1 ? 'Active' : 'Inactive' }}</td>
-                            <td class="text-center">
-                                <button class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true"
-                                    aria-expanded="false">
-                                    <i class="fa fa-ellipsis-v"></i>
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="{{ route('admin.branch.edit', $branch->id) }}">
-                                        <i class="fa fa-edit"></i>
-                                    </a>
-
-                                    <a class="dropdown-item text-danger"
-                                        href="{{ route('admin.branch.delete', $branch->id) }}">
-                                        <i class="fa fa-trash"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="5" class="text-center">No records found</td>
-                        </tr>
-                    @endforelse --}}
-                </tbody>
-            </table>
+    </div>
+    <div class="d-flex justify-content-between align-items-baseline flex-wrap grid-margin">
+        <div class="tab-list mb-3 col-md-8">
+            <button class="main-btn ">Recent Active</button>
+            <button class="main-btn">Current Active</button>
+            <button class="main-btn active">Canceled</button>
+            <button class="main-btn ">Unconfirmed</button>
         </div>
-        <div class="card shadow-sm mx-auto">
-            <!-- SweetAlert trigger -->
-            @if (session('success'))
-                <script>
-                    // Show the Toast
-                    showToast('success', '{{ session('success') }}', '#d4edda');
-                </script>
-            @endif
+        <div class="d-flex align-items-">
+            <form class="search-form">
+                <div class="input-group flex-nowrap w-100">
+                    <div class="input-group-text me-2" style="background: transparent!important; border:0;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        </svg>
+                    </div>
+                    <input type="text" class="form-srch w-100" id="navbarForm" placeholder="Search By Room Number">
+                </div>
+            </form>
         </div>
     </div>
+    <div class="row card col-md-12">
+        <table class="table table-responsive">
+            <thead>
+                <tr>
+                    <th>Name/Company</th>
+                    <th>Plan</th>
+                    <th>Price</th>
+                    <th>Payment Method</th>
+                    <th class="text-center">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <img class="wd-30 ht-30 rounded-circle" src="{{ asset('adminPanel/images/favicon-1.png') }}"" alt=" profile">
+                        Yasir/Bridge
+                    </td>
+                    <td>Free</td>
+                    <td>0.00 USD</td>
+                    <td>Paypall</td>
+                    <td class="text-center">
+                        <span class="label label-book">Confirmed</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img class="wd-30 ht-30 rounded-circle" src="{{ asset('adminPanel/images/favicon-1.png') }}"" alt=" profile">
+                        Yasir/Bridge
+                    </td>
+                    <td>Free</td>
+                    <td>0.00 USD</td>
+                    <td>Paypall</td>
+                    <td class="text-center">
+                        <span class="label label-cancel">Confirmed</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img class="wd-30 ht-30 rounded-circle" src="{{ asset('adminPanel/images/favicon-1.png') }}"" alt=" profile">
+                        Yasir/Bridge
+                    </td>
+                    <td>Free</td>
+                    <td>0.00 USD</td>
+                    <td>Paypall</td>
+                    <td class="text-center">
+                        <span class="label label-pending">Confirmed</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="card shadow-sm mx-auto">
+        <!-- SweetAlert trigger -->
+        @if (session('success'))
+        <script>
+            // Show the Toast
+            showToast('success', '{{ session('success ') }}', '#d4edda');
+        </script>
+        @endif
+    </div>
+</div>
 @endsection
