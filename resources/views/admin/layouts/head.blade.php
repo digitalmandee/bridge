5a9199945132450fc6f3f7e2f850d0a2a7bd9d90
yasirCodes12 <122172335+yasirCodes12@users.noreply.github.com>
2024-10-30 21:05:09 +0500

    show invoice layout

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -32,6 +32,7 @@
 
     <!-- Custom Css -->
     <link rel="stylesheet" href="{{ asset('custom/custom.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
