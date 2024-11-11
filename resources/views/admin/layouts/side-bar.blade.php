dcc21ffae652a9362e566d2de732363d9a74585a
Sulman19 <119170589+Sulman19@users.noreply.github.com>
2024-11-11 18:33:48 +0500

    sidebar active class fixes.

--- resources/views/admin/layouts/side-bar.blade.php
+++ resources/views/admin/layouts/side-bar.blade.php
@@ -13,15 +13,15 @@
         <ul class="nav">
             @can('dashboard')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('dashboard') ? 'active' : 'non-active' }}">
-                <a href="{{ route('dashboard') }}" class="nav-link bar-link">
+                <a href="{{ route('dashboard') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/dashboard.svg') }}" alt="dashboard Icon">
                     <span class="link-title">Dashboard</span>
                 </a>
             </li>
-            @endcan()
+            @endcan
             @can('branches')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.branches', 'admin.branches.create', 'admin.branch.edit') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.branches') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.branches') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/branches.svg') }}" alt="Branches Icon">
                     <span class="link-title">Branches</span>
                 </a>
@@ -29,7 +29,7 @@
             @endcan
             @can('branch-manager')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.branch.manager', 'admin.branch.manager.create', 'admin.branch.manager.edit') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.branch.manager') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.branch.manager') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/branch-manager.svg') }}" alt="Branch-Manager Icon">
                     <span class="link-title">Branch Manager</span>
                 </a>
@@ -37,7 +37,7 @@
             @endcan
             @can('products')
             <li class="nav-item bar-item mb-1">
-                <a href="#" class="nav-link bar-link">
+                <a href="#" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/product.svg') }}" alt="Products Icon">
                     <span class="link-title">Products</span>
                 </a>
@@ -45,7 +45,7 @@
             @endcan
             @can('floor-plan')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.floor.plan') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.floor.plan') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.floor.plan') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/floorplan.svg') }}" alt="Floor Icon">
                     <span class="link-title">Floor Plan</span>
                 </a>
@@ -53,7 +53,7 @@
             @endcan
             @can('inventory-management')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.inventory.management') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.inventory.management') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.inventory.management') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/inventory.svg') }}" alt="Inventory Icon">
                     <span class="link-title">Inventory Management</span>
                 </a>
@@ -61,7 +61,7 @@
             @endcan
             @can('booking-management')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.booking.calendar', 'admin.booking.create') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.booking.calendar') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.booking.calendar') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/booking.svg') }}" alt="Booking Icon">
                     <span class="link-title">Booking Management</span>
                 </a>
@@ -69,7 +69,7 @@
             @endcan
             @can('expense-management')
             <li class="nav-item bar-item mb-1">
-                <a href="#" class="nav-link bar-link">
+                <a href="#" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/expenseside.svg') }}" alt="Expense Icon">
                     <span class="link-title">Expense Management</span>
                 </a>
@@ -77,7 +77,7 @@
             @endcan
             @can('financial-report')
             <li class="nav-item bar-item mb-1">
-                <a href="#" class="nav-link bar-link">
+                <a href="#" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/finalcial.svg') }}" alt="Financial Icon">
                     <span class="link-title">Financial Report</span>
                 </a>
@@ -85,7 +85,7 @@
             @endcan
             @can('revenue-check')
             <li class="nav-item bar-item mb-1">
-                <a href="#" class="nav-link bar-link">
+                <a href="#" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/revenue.svg') }}" alt="Revenue Icon">
                     <span class="link-title">Revenue Check</span>
                 </a>
@@ -93,7 +93,7 @@
             @endcan
             @can('roles')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.roles', 'admin.roles.create', 'admin.roles.edit') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.roles') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.roles') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/Role.svg') }}" alt="Settings Icon">
                     <span class="link-title">Roles</span>
                 </a>
@@ -101,7 +101,7 @@
             @endcan
             @can('permissions')
             <li class="nav-item bar-item mb-1 {{ Request::routeIs('admin.permissions', 'admin.permissions.create', 'admin.permissions.edit') ? 'active' : 'non-active' }}">
-                <a href="{{ route('admin.permissions') }}" class="nav-link bar-link">
+                <a href="{{ route('admin.permissions') }}" class="nav-link bar-item bar-link">
                     <img class="link-icon" src="{{ asset('icons/permission.svg') }}" alt="Settings Icon">
                     <span class="link-title">Permissions</span>
                 </a>
