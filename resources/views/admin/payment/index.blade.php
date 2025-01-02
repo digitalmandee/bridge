4337a594bff5d56a5dacca015e65556100ebfe56
myali1 <Yasir.ali@digitalmandee.com>
2025-01-02 10:44:20 +0500

    update the payment

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -28,43 +28,4 @@
         </div>
     </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <!-- Modal -->
-        <div class="modal-content">
-            <form>
-                <p class="border-bottom">Payment</p>
-                <div class="d-flex gap-3">
-                    <label><input type="radio" name="payment-method" value="card" checked> Card</label>
-                    <label><input type="radio" name="payment-method" value="bank"> Bank</label>
-                    <label><input type="radio" name="payment-method" value="transfer"> Transfer</label>
-                </div>
-                <div>
-                    <label for="card-number">Card Number</label>
-                    <input class="form-control" type="text" id="card-number" placeholder="1234 5678 9101 1121">
-                </div>
-                <div class="d-flex">
-                    <div class="expiration-date">
-                        <label for="expiration-date">Expiration Date</label>
-                        <input class="form-control" type="text" id="expiration-date" placeholder="MM/YY">
-                    </div>
-                    <div class="cvv-number">
-                        <label for="cvv">CVV</label>
-                        <input class="form-control" type="text" id="cvv" placeholder="123">
-                    </div>
-                </div>
-                <div class="d-flex align-items-baseline">
-                    <span style="width:10%;"><input type="checkbox"></span>
-                    <label>Save card details</label>
-                </div>
-                <button type="submit">Pay USD59.28</button>
-
-            </form>
-            <p>Your personal data will be used to process your order and for other purposes described in our privacy
-                policy.</p>
-        </div>
-    </div>
-</div>
 @endsection
