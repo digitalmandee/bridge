bed78e24131f0314c9a8b6cadbea9f603db7e2ff
Muhammad Fahad <mfd84739@gmail.com>
2025-03-13 16:31:07 +0500

    company links updated

--- src/pages/company/addstaff.jsx
+++ src/pages/company/addstaff.jsx
@@ -9,6 +9,8 @@ import axiosInstance from "@/utils/axiosInstance";
 
 const AddStaff = () => {
 	const navigate = useNavigate();
+	const { branch } = useParams();
+
 	const fileInputRef = useRef(null);
 	const [avaiablePrintingQuota, setAvaiablePrintingQuota] = useState(0);
 	const [avaiableBookingQuota, setAvaiableBookingQuota] = useState(0);
@@ -118,7 +120,7 @@ const AddStaff = () => {
 				setBookingSeats((prevSeats) => prevSeats.filter((seat) => seat.id != seatNo));
 
 				// Optional: Redirect to staff management page or show success message
-				navigate("/company/staff/management");
+				navigate(`/${branch}/company/staff/management`);
 			} else {
 				console.log("Error in staff creation:", res.data.message);
 			}
